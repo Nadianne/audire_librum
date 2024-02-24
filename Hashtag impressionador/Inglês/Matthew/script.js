33d2f5a9c0a7f8e07a7285413ff831608074383f
Nadianne Galvão <54487179+Nadianne@users.noreply.github.com>
2024-02-24 18:00:33 -0300

    matthew-and-jude

---
+++ Hashtag impressionador/Inglês/Matthew/script.js
@@ -0,0 +1,68 @@
+const botaoPlayPause = document.getElementById('botaoPlayPause');
+const audioCapitulo = document.getElementById('audioCapitulo');
+const botaoNext = document.getElementById('botaoNext');
+const botaoBack = document.getElementById('botaoBack');
+const nomeCapitulo = document.getElementById('capitulo');
+
+const num_cap = 28;
+let player = 0;
+let capAtual = 1;
+
+function tocarFaixa(){
+    audioCapitulo.play();
+    botaoPlayPause.classList.remove('bi-play');
+    botaoPlayPause.classList.add('bi-pause');
+}
+
+function pausarFaixa(){
+    audioCapitulo.pause();
+    botaoPlayPause.classList.add('bi-play')
+    botaoPlayPause.classList.remove('bi-pause')
+}
+
+function playOrPause(){
+    if(player === 0){
+        tocarFaixa();
+        player = 1;
+    } else {
+        pausarFaixa();
+        player = 0;
+    }
+}
+
+function trocarNome(){
+    nomeCapitulo.innerText = 'Chapter ' + capAtual;
+}
+
+function proxFaixa(){
+    if (capAtual === num_cap){
+        capAtual =1;
+    } else{
+        capAtual = capAtual + 1
+    }
+
+    
+    audioCapitulo.src = "the_gospel_according_to_st_matthew_ss_librivox/" + capAtual + '.mp3';
+    tocarFaixa();
+    player = 1;
+    trocarNome();
+}
+
+
+function voltarFaixa(){
+    if (capAtual === 1){
+        capAtual =1;
+    } else{
+        capAtual = capAtual - 1;
+    }
+
+    
+    audioCapitulo.src = "the_gospel_according_to_st_matthew_ss_librivox/" + capAtual + '.mp3';
+    tocarFaixa();
+    player = 1;
+    trocarNome();
+}
+
+botaoBack.addEventListener('click', voltarFaixa)
+botaoNext.addEventListener('click', proxFaixa)
+botaoPlayPause.addEventListener('click', playOrPause);
